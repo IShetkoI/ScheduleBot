166598e9949d734cdff320b7c94846e5c5a6abee
IShetkoI <vanya.shetko38@gmail.com>
2021-09-21 23:53:46 +0300

    make it better

--- handlers/commands/give_me.py
+++ handlers/commands/give_me.py
@@ -7,4 +7,8 @@ from aiogram import types
 async def give_me(message: types.Message):
     args = message.get_args()
     await dp.bot.delete_message(message.chat.id, message.message_id)
-    await dp.bot.forward_message(chat_id=-1001250101317, from_chat_id=message.chat.id, message_id=config.lib[args][0])
+    print(config.lib[args][0])
+    print(config.lib[args])
+    print(config.lib)
+
+    await dp.bot.forward_message(chat_id=message.chat.id, from_chat_id=-1001250101317, message_id=config.lib[args][0])
